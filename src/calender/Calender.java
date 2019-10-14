c00d267ffc60cb47fb9cd81a800877aef516ed99
User <User@User-PC7853356>
2019-10-14 13:15:25 +0900

    Update calender main()

--- src/calender/Calender.java
+++ src/calender/Calender.java
@@ -2,11 +2,16 @@ package calender;
 
 public class Calender {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		System.out.println("Hello Calender!");
-
+	public static void main(String args[]){
+		System.out.println("일 월 화 수 목 금 토");
+		System.out.println("--------------------");
+		int date = 0;
+		for(int i = 0; i < 4; i++){
+			for(int j = 0; j < 7; j++){
+				date++;
+				System.out.print(date + " ");
+			}
+			System.out.println("");
+		}
 	}
-
 }
